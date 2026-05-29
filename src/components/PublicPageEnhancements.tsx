7fbfa206706cb96aad7a1b5713deb5e83b4bea62
indijan <indijanmac@gmail.com>
2026-05-29 18:42:16 +1200

    still page speed

--- src/components/PublicPageEnhancements.tsx
+++ src/components/PublicPageEnhancements.tsx
@@ -1,16 +1,18 @@
 import { cookies } from "next/headers";
-import PublicAnalyticsMount from "@/components/PublicAnalyticsMount";
 import PublicCookieNotice from "@/components/PublicCookieNotice";
 import { PUBLIC_COOKIE_NOTICE_KEY } from "@/lib/publicShell";
 
 export default async function PublicPageEnhancements() {
     const cookieStore = await cookies();
     const hasConsent = cookieStore.get(PUBLIC_COOKIE_NOTICE_KEY)?.value === "1";
+    const PublicAnalyticsMount = hasConsent
+        ? (await import("@/components/PublicAnalyticsMount")).default
+        : null;
 
     return (
         <>
             <PublicCookieNotice initialDismissed={hasConsent} />
-            <PublicAnalyticsMount initialConsent={hasConsent} />
+            {PublicAnalyticsMount ? <PublicAnalyticsMount initialConsent={hasConsent} /> : null}
         </>
     );
 }
